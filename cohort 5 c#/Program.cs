48dc20a72a57c8624f16b84af3ef501756c75052
surahailat <hailatsura@gmail.com>
2025-01-19 21:26:53 +0300

    task2

--- cohort 5 c#/Program.cs
+++ cohort 5 c#/Program.cs
@@ -11,94 +11,245 @@ namespace cohort_5_c_
     {
         static void Main(string[] args)
         {
-            //1 - Create console application that read string from user and print the same string
+            ////1 - Create console application that read string from user and print the same string
 
-            string name = Console.ReadLine();
-            Console.WriteLine("HELLO  " + name);
-
-
-
-            //            2 - Define variables with kind of
-            //a.Double
-            //b.String
-            //c.Char
-            //d.Bool
-            //e.Int
-            //f.Const
-            //Then display it in console application
-
-            double price = 15.55;
-            string name1 = "sura";
-            char a = 'A';
-            bool boolian = false;
-            int number = 50;
-            const float num = 3.14f;
-
-            Console.WriteLine(name1);
+            //string name = Console.ReadLine();
+            //Console.WriteLine("HELLO  " + name);
 
 
 
-            //3 - Define array with name car that hold more than 3 kinds on cars, then print all the cars with the array length.
+            ////            2 - Define variables with kind of
+            ////a.Double
+            ////b.String
+            ////c.Char
+            ////d.Bool
+            ////e.Int
+            ////f.Const
+            ////Then display it in console application
 
-            string[] cars = { "BMW", "KIA", "HONDA" };
-            foreach (var i in cars) // i ==> value
+            //double price = 15.55;
+            //string name1 = "sura";
+            //char a = 'A';
+            //bool boolian = false;
+            //int number = 50;
+            //const float num = 3.14f;
+
+            //Console.WriteLine(name1);
+
+
+
+            ////3 - Define array with name car that hold more than 3 kinds on cars, then print all the cars with the array length.
+
+            //string[] cars = { "BMW", "KIA", "HONDA" };
+            //foreach (var i in cars) // i ==> value
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+            ////  4 - Write a program in C that reads a first name, Lastname and year of birth and display the names and the year one after another sequentially.Go to the editor
+            ////•	Input your firstname: Sami
+            ////•	Input your lastname: Ali
+            ////•	Input your year of birth: 1999
+            ////•	Sami Ali 1999
+
+
+
+            //Console.Write("Input Your First Name: ");
+            //string FirstName = Console.ReadLine();
+
+
+            //Console.Write("Input Your Last Name: ");
+            //string LastName = Console.ReadLine();
+
+
+            //Console.Write("Input Your Date of Birth: ");
+            //int birth = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine(FirstName+" "+ LastName+" "+ birth);
+
+
+
+
+            ////   5 - Write a program in C # to store elements in an array and print it. 
+            ////•	Test Data :	
+            ////•	Input 10 elements in the array :
+            ////•	element - 0 : 1
+            ////•	element - 1 : 1
+            ////•	element - 2 : 2
+            ////•	.......
+            ////•	Expected Output :
+            ////•	Elements in array are: 1 1 2 3 4 5 6 7 8 9
+
+
+            //int[] elements = new int[10];
+            //Console.WriteLine("Enter 10 elements:");
+
+            //for (int i = 0; i < elements.Length; i++)
+            //{
+
+            //    elements[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Array elements: ");
+            //for (int i = 0; i < elements.Length; i++)
+            //{
+            //    Console.Write(elements[i] + " ");
+            //}
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("age " + num);
+
+
+            //double number = 5.5555;
+            //string fname = "sura";
+            //char character = 'l';
+            //bool boolian = true;
+            //int intnum = 10;
+            //const float pi = 3.14f;
+            //Console.WriteLine(number);
+            //Console.WriteLine(fname);
+            //Console.WriteLine(character);
+            //Console.WriteLine(boolian);
+            //Console.WriteLine(intnum);
+            //Console.WriteLine(pi);
+
+
+            //string[] car = { "bmw", "mercedes", "honda" };
+
+            //foreach (var i in car)
+            //{
+            //    Console.WriteLine(i);
+
+            //};
+            //car[0] = "golf";
+            //foreach (var i in car)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //string firstName = Console.ReadLine();
+            //Console.Write(firstName);
+
+            //string lastName = Console.ReadLine();
+            //Console.Write(firstName);
+
+            //int birth = Convert.ToInt32(Console.ReadLine());
+            //Console.Write(birth);
+
+
+            //int[] arr = new int[10];
+            //Console.WriteLine("Enter the element: ");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //   arr[i]= Convert.ToInt32(Console.ReadLine());
+            //}
+            //foreach (var i in arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //string[] fruits = new string[5];
+            //Console.WriteLine("write 5 fruits: ");
+            //for (int i = 0; i < fruits.Length; i++)
+            //{
+            //    fruits[i] = Console.ReadLine();
+            //}
+            //foreach (var i in fruits)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine(fruits.Length);
+
+
+
+
+
+            string[] colors ={ "Red", "Blue", "Green", "Yellow", "Rose", "Purple", "black", "Ruby"};
+
+            string color = "";
+            int count = 0;
+
+            foreach (string co in colors)
             {
-                Console.WriteLine(i);
+                if (co.StartsWith("R", StringComparison.OrdinalIgnoreCase))
+                {
+                    color += co + ' ';
+                    count++;
+
+                }
+            }
+            if (count>0)
+                {
+                    Console.WriteLine("colors starting with 'R' :"+color);
+                    
+                }
+            Console.WriteLine("Total count :" + count);
+            }
+        
+        
+
+            int[] numbers1 = new int[6];
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                numbers1[i]= Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            float sum = 0;
+            foreach (int element in numbers1)
+            {
+                sum += element;
+            }
+            Console.WriteLine("Sum of elements : " + sum);
+            Console.WriteLine("Avarege of elements : "+sum/(numbers1.Length));
+
+            int min = numbers1[0];
+            foreach (var item in numbers1)
+            {
+                if (item<min)
+                {
+                    min = item; 
+                }
+            }
+            Console.WriteLine("Minimum value :" + min);
+
+
+            int max = numbers1[0];
+            foreach (var i in numbers1)
+            {
+                if (i>max)
+                {
+                    max = i;
+                }
+            }
+            Console.WriteLine("Maximam value : " + max );
+
+
+            Array.Sort(numbers1);
+            Console.Write("sort");
+            foreach (var x in numbers1)
+            {
+                Console.Write(" "+x);
             }
 
 
 
 
-            //  4 - Write a program in C that reads a first name, Lastname and year of birth and display the names and the year one after another sequentially.Go to the editor
-            //•	Input your firstname: Sami
-            //•	Input your lastname: Ali
-            //•	Input your year of birth: 1999
-            //•	Sami Ali 1999
-
-
-            
-            Console.Write("Input Your First Name: ");
-            string FirstName = Console.ReadLine();
-
-            
-            Console.Write("Input Your Last Name: ");
-            string LastName = Console.ReadLine();
-
-
-            Console.Write("Input Your Date of Birth: ");
-            int birth = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.Write("");
 
 
 
 
-            //            5 - Write a program in C # to store elements in an array and print it. 
-            //•	Test Data :	
-            //•	Input 10 elements in the array :
-            //•	element - 0 : 1
-            //•	element - 1 : 1
-            //•	element - 2 : 2
-            //•	.......
-            //•	Expected Output :
-            //•	Elements in array are: 1 1 2 3 4 5 6 7 8 9
 
 
-            int[] elements = new int[10];
-            Console.WriteLine("Enter 10 elements:");
 
-            for (int i = 0; i < elements.Length; i++)
-            {
 
-                elements[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.WriteLine("Array elements: ");
-            for (int i = 0; i < elements.Length; i++)
-            {
-                Console.Write(elements[i] + " ");
-            }
 
 
 
---
+++ project2-19-1/Program.cs
@@ -0,0 +1,290 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace project2_19_1
+{
+    internal class Program
+    {
+        static int smaller(int x,int y)
+        {
+            if (x>y)
+            {
+                return y;
+            }
+            else
+            {
+                return x;   
+            }
+        }
+
+
+        static double speed()
+        {
+           double value=Convert.ToInt32 (Console.ReadLine());
+            double val= value*0.621371;
+            return val;
+
+        }
+
+        static int clock()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            string s = $"{a} hours, {b} minutes";
+            int sum = (a*60)+b;
+            return sum;
+
+        }
+
+        static string clock2()
+        {
+            int mins = Convert.ToInt32(Console.ReadLine());
+            int h = mins / 60;
+            int secs = mins % 60;
+            string oclock = ($"{h} Hours , {secs} Minutes");
+            return oclock;
+        }
+
+
+        static string Compare(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return "Equal";
+            }
+            else if (num1 > num2)
+            {
+                return "Greater";
+            }
+            else
+            {
+                return "Smaller";
+            }
+        }
+
+
+
+
+        static int sumnum(int num0)
+        {
+            int sumn = 0;
+            int divi;
+            while (num0>0)
+            {
+                divi = num0 % 10;
+                sumn += divi;
+                num0 = num0/10;
+            }
+            return sumn;
+        }
+
+
+
+
+
+        
+
+
+
+
+
+
+            static void Main(string[] args)
+        {
+
+            // 1 - Write a C# program that accepts a double input and converts it into an int. Display both values.
+            //•	Example:
+            // Input: 9.8
+
+            double dub = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine(dub);
+            Console.WriteLine((int)dub);
+
+
+            //2 - Write a program that uses a Convert.ToString() method to convert a number into a string and concatenate it with a sentence.
+            //Example:
+            //Input: 25
+            //Output: "Your number is: 25"
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is: "+ Convert.ToString(num));
+
+
+            //3 - Create a string with a sentence. Use ToUpper() and ToLower() to display the uppercase and lowercase versions of the string.
+            //Input: "C# is fun!"
+
+            string sentence = Console.ReadLine();
+            Console.WriteLine("Uppercase: C# IS "+ sentence.ToUpper());
+            Console.WriteLine("Lowercase: c# is " + sentence.ToLower());
+
+
+//            4 - Write a program that takes a user's full name as input and prints:
+//The first name.
+//The last name.
+//The length of the full name.
+//•	Example:
+//        Input: "John Doe"
+
+            string fName =Console.ReadLine();
+            string LName = Console.ReadLine();
+            string fullName = fName+" "+ LName;
+            Console.WriteLine(fullName);
+            Console.WriteLine("First Name: "+fName);
+            Console.WriteLine("Last Name: "+LName);
+            Console.WriteLine(fullName.Length);
+
+
+            //5 - Write a program that accepts two integers and prints the smaller of the two.
+            //Input: 5, 9
+
+            Console.WriteLine(smaller(5,9 ));
+
+
+            //6 - Write a method that converts kilometers per hour to miles per hour.Use this method in a program to convert and display the result.
+            //Input: 15 km / h
+            //Output: "9.320568 mph"
+
+            Console.WriteLine(speed()+ " mph");
+
+            //7 - Write a method that takes hours and minutes as input and returns the total number of minutes.
+            //Input: 5 hours, 37 minutes
+
+            Console.WriteLine("Total: "+clock()+ " minutes ");
+
+
+
+            // 8 - Write a method that accepts minutes as input and calculates the total number of hours and minutes.
+            //•	Input: 546 minutes
+
+            Console.WriteLine(clock2());
+
+            //9 - Compare Two Numbers
+            //Write a program that prints:
+            //            "Equal" if two numbers are the same.
+            //"Greater" if the first is greater.
+            //"Smaller" if the second is greater.
+            //Use only comparison and ternary operators.
+            //•	Input: 10, 20
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine (Compare(num1, num2));
+
+
+            //  Write a program that calculates the sum of the digits of a number using arithmetic operators and loops(no conditionals).
+            //Input: 123
+            //Output: "Sum of digits: 6"
+
+
+            int num0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine (sumnum(num0));
+
+
+            //10 - Reverse a Number
+            //Write a program that reverses a number using arithmetic operators and loops.
+            //Input: 1234
+
+            int[] arr = { 1, 2, 3, 4 };
+            
+            for (int i = arr.Length-1; i >=0 ; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+           
+
+
+
+
+            //11 - Check Divisibility
+            //Write a program to check if a number is divisible by another number using logical and arithmetic operators. Return a message "Divisible" or "Not Divisible".
+            //Input: 25, 5
+
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            
+                if (number1 % number2 == 0)
+                {
+                    Console.WriteLine( "Divisible");
+                }
+                else
+                {
+                    Console.WriteLine( "Not Divisible");
+                }
+
+
+
+//            12 - Find the Middle Value
+//Write a program to find the middle value of three numbers using arithmetic and comparison operators.
+
+
+                int firstnum=Convert.ToInt32(Console.ReadLine());
+            int midnum=Convert.ToInt32(Console.ReadLine());
+            int finalnum =Convert.ToInt32(Console.ReadLine());
+            int sort;
+            if (firstnum>midnum)
+            {
+                sort = firstnum;
+                firstnum = midnum;
+                midnum = sort;
+            }
+            if (midnum>finalnum)
+            {
+                sort = midnum;
+                midnum = finalnum;
+                finalnum = sort;
+            }
+            if (firstnum > midnum)
+            {
+                sort = firstnum;
+                firstnum = midnum;
+                midnum = sort;
+            }
+
+
+            //Console.WriteLine($"{firstnum}, {midnum}, {finalnum}");
+            Console.WriteLine($"The middle value is:  {midnum}");
+
+
+
+            //Console.WriteLine("Enter three numbers:");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+           
+            //int middle;
+
+            //if ((num1 > num2 && num1 < num3) || (num1 > num3 && num1 < num2))
+            //{
+            //    middle = num1;
+            //}
+            //else if ((num2 > num1 && num2 < num3) || (num2 > num3 && num2 < num1))
+            //{
+            //    middle = num2;
+            //}
+            //else
+            //{
+            //    middle = num3;
+            //}
+
+           
+            //Console.WriteLine($"The middle value is: {middle}");
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+}
